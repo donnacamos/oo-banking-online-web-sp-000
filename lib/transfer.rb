475c06321e592c38bf27bb2997510a3346585311
Donna Amos <donnacamos.developer@gmail.com>
2019-03-20 00:27:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,5 +18,7 @@ class Transfer
     if @sender.balance > @amount && @status == "pending" 
       @sender.balance -= @amount
       @receiver.balance += @amount 
-      
+      @status = "complete" 
+    else 
+      @
 end
