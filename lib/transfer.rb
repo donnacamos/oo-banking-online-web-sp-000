5b674a0e6b5518d0deee7f71e6f50e616b7ceea0
Donna Amos <donnacamos.developer@gmail.com>
2019-03-20 00:24:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -4,4 +4,10 @@ class Transfer
   attr_accessor :sender, :receiver, :amount, :status 
   
   def initialize(sender, receiver, amount)
+    @sender = sender 
+    @receiver = receiver 
+    @amount = amount 
+    @status = "pending" 
+  end 
+  
 end
