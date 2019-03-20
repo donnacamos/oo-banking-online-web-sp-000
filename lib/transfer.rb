d223af729203854e843de5db2aa96554cc1ff497
Donna Amos <donnacamos.developer@gmail.com>
2019-03-20 00:38:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,8 +27,8 @@ end
 
   def reverse_transfer 
     if @status == "complete" 
-      @sender.balance -= @amount 
-      @receiver.balance += @amount 
+      @sender.balance += @amount 
+      @receiver.balance -= @amount 
       @status = "reversed"  
     end 
   end 
